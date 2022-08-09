2d6e317eec260a9f5fce43cfae55e9300fdf7cdb
Azizul109 <mdazizularif109@gmail.com>
2022-08-09 19:44:44 +0600

    seo handle and api call

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 
-const index = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const index = ({ data }) => {
     return (
-        <div>
-            <Navbar></Navbar>
-            <h1>this is blog index page</h1>
-        </div>
+      <div>
+        <Navbar></Navbar>
+        {data.slice(0, 4).map((curElem) => {
+          return (
+            <div key={curElem.id} className="ssr-styles">
+              <h3>{curElem.id}</h3>
+              <h2>{curElem.title}</h2>
+            </div>
+          );
+        })}
+      </div>
     );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
 import styles from "../styles/index.module.css";
+import Head from 'next/head';
 
 const index = () => {
   return (
     <>
+      <Head>
+        <title>Home Page </title>
+        <meta name="description" content="Free Web youtube tutorials" />
+        <meta name="keywords" content="HTML, CSS, JavaScript, next.js" />
+        <meta name="author" content="arif" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
       <header className={styles.header}>
-        <div className='brand_box'>
+        <div className="brand_box">
           <Navbar></Navbar>
         </div>
 
